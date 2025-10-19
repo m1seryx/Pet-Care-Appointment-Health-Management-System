9833d5e8d0fe9935544f53b3e40099711c422b6d
Rashdy Arobie <mohrashdyarobi@gmail.com>
2025-10-19 23:24:07 +0800

    Updated project and added LightRays component

--- front-end/pet-care/src/components/Login.jsx
+++ front-end/pet-care/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import dogBg from "./dog.jpg"; 
 import "./Login.css";
+import LightRays from "./LightRays";
 
 export default function Login() {
   const navigate = useNavigate();
@@ -20,28 +20,37 @@ export default function Login() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
     if (!formData.username || !formData.password) {
       alert("Please fill out all fields!");
       return;
     }
-
     console.log("Login successful:", formData);
-
     navigate("/");
   };
 
   return (
-    <div className="signin-container" style={{ backgroundImage: `url(${dogBg})` }}>
+    <div className="signin-container">
       <div className="overlay"></div>
-      <div className="form-section">
+
+      { }
+      <div style={{ width: "100%", height: "100%", position: "absolute", zIndex: 1 }}>
+        <LightRays
+          raysOrigin="top-center"
+          raysColor="#00ffff"
+          raysSpeed={1.5}
+          lightSpread={0.8}
+          rayLength={1.2}
+          followMouse={true}
+          mouseInfluence={0.1}
+          noiseAmount={0.1}
+          distortion={0.05}
+          className="custom-rays"
+        />
+      </div>
+
+      <div className="form-section" style={{ position: "relative", zIndex: 2 }}>
         <div className="form-wrapper">
           <div className="logo">
-            <div className="logo-icon">
-              <svg viewBox="0 0 24 24">
-                <path d="M12 2C10.34 2 9 3.34 9 5C9 6.66 10.34 8 12 8C13.66 8 15 6.66 15 5C15 3.34 13.66 2 12 2M5 8C3.34 8 2 9.34 2 11C2 12.66 3.34 14 5 14C6.66 14 8 12.66 8 11C8 9.34 6.66 8 5 8M19 8C17.34 8 16 9.34 16 11C16 12.66 17.34 14 19 14C20.66 14 22 12.66 22 11C22 9.34 20.66 8 19 8M12 9C8.5 9 4 10.76 4 14V16H20V14C20 10.76 15.5 9 12 9Z" />
-              </svg>
-            </div>
             <span>PetCare</span>
           </div>
 
